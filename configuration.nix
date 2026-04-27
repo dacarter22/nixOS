eba5b8f04b2a306b93fbf19f7435b7854ea8e369
dacarter22 <davidacarter22@gmail.com>
2026-04-26 22:56:17 -0400

    extra

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,39 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+
+{
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  time.timeZone = "America/NewYork";
+  time.timeZone = "America/New_York";
 
+  # Enable GUI
+  services.xserver.enable = true;
+
+  # GNOME
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false; # IMPORTANT for VM/NOS3
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Audio (modern)
+  sound.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Docker
   virtualisation.docker.enable = true;
 
+  # User
   users.users.user = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "docker" ];
   };
 
+  # Packages
   environment.systemPackages = with pkgs; [
     nano
     vim
